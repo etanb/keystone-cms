38fbcc43a1ac4b774330fa43f5c0cda1d8d1bb18
renagadex2 <etan.berkowitz@gmail.com>
2015-05-17 17:09:47 -0700

    yoga rendering phase .1

--- routes/views/yoga.js
+++ routes/views/yoga.js
@@ -4,6 +4,22 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res),
 		locals = res.locals;
+
+  locals.data = {
+    yogasections: []
+  };
+
+
+  view.on('init', function(next) {
+    
+    var q = keystone.list('YogaSection').model.find().where('state', 'published');
+    
+    q.exec(function(err, results) {
+      locals.data.yogasections = results;
+      next(err);
+    });
+    
+  });
 	
 	// Render the view
 	view.render('yoga');
